ae95bc9fba013bf18ca5c92931efa6ee37dbfd68
it69070002 <69070002@kmitl.ac.th>
2026-07-09 22:23:43 +0700

    add : color

--- oj[3011]/Colors.py
+++ oj[3011]/Colors.py
@@ -9,6 +9,12 @@ def main():
         color = "Violet"
     elif (color_a) == "Yellow" and (color_b) == "Blue":
         color = "Green"
+    elif (color_a) == "Yellow" and (color_b) == "Yellow":
+        color = "Yellow"
+    elif (color_a) == "Blue" and (color_b) == "Blue":
+        color = "Blue"
+    elif (color_a) == "Red" and (color_b) == "Red":
+        color = "Red"
     else:
         color = "Error"
     print(color)
