b0e9d6416d41c54826ab0980b7d146c96a75f261
it69070002 <69070002@kmitl.ac.th>
2026-07-09 22:41:03 +0700

    Update Colors.py

--- oj[3011]/Colors.py
+++ oj[3011]/Colors.py
@@ -3,17 +3,17 @@ def main():
     """main"""
     colora = input()
     colorb = input()
-    if ((colora) == "Red" and (colorb) == "Yellow") or ((colora) == "Yellow" and (colorb) == "Red"):
+    if((colora) == "Red" and (colorb) == "Yellow") or ((colora) == "Yellow" and (colorb) == "Red"):
         color = "Orange"
-    elif ((colora) == "Red" and (colorb) == "Blue") or ((colora) == "Blue" and (colorb) == "Red"):
+    elif((colora) == "Red" and (colorb) == "Blue") or ((colora) == "Blue" and (colorb) == "Red"):
         color = "Violet"
-    elif ((colora) == "Yellow" and (colorb) == "Blue") or ((colora) == "Blue" and (colorb) == "Yellow"):
+    elif((colora) == "Yellow" and (colorb) == "Blue") or ((colora) == "Blue" and (colorb) == "Yellow"):
         color = "Green"
-    elif (colora) == "Yellow" and (colorb) == "Yellow":
+    elif(colora) == "Yellow" and (colorb) == "Yellow":
         color = "Yellow"
-    elif (colora) == "Blue" and (colorb) == "Blue":
+    elif(colora) == "Blue" and (colorb) == "Blue":
         color = "Blue"
-    elif (colora) == "Red" and (colorb) == "Red":
+    elif(colora) == "Red" and (colorb) == "Red":
         color = "Red"
     else:
         color = "Error"
